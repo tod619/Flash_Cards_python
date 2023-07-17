070a595b3de608c1b685bc42bcc61c4543396c22
tod619 <trevorodwyer619@hotmail.com>
2023-07-17 22:28:34 +0100

    Create known & unknown btns in main.py file

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ BACKGROUND_COLOR = "#B1DDC6"
 # Create Window
 window = Tk()
 window.title("Flash Cards")
-window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+window.config(padx=40, pady=40, bg=BACKGROUND_COLOR)
 
 # Add card image to canvas
 canvas = Canvas(width=800, height=526)
@@ -16,6 +16,15 @@ card_front_img = PhotoImage(file="images/card_front.png")
 canvas.create_image(400, 263, image=card_front_img)
 canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
 canvas.create_text(400, 263, text="Word", font=("Ariel", 60, "bold"))
-canvas.grid(row=0, column=0)
+canvas.grid(row=0, column=0, columnspan=2)
+
+# Buttons
+cross_img = PhotoImage(file="images/wrong.png")
+unknown_button = Button(image=cross_img, highlightthickness=0)
+unknown_button.grid(row=1, column=0)
+
+check_img = PhotoImage(file="images/right.png")
+known_button = Button(image=check_img, highlightthickness=0)
+known_button.grid(row=1, column=1)
 
 window.mainloop()
