f06b04a29ee4bf0c5ee255274b5ce14760422b51
tod619 <trevorodwyer619@hotmail.com>
2023-07-17 21:57:30 +0100

    Create main window for GUI in main.py file

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 # Flash Cards
 # 17/07/2023
+from tkinter import *
 
+BACKGROUND_COLOR = "#B1DDC6"
+
+window = Tk()
+window.title("Flash Cards")
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+window.mainloop()
