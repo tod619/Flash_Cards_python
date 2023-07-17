4e347f154179f229589a609944ed451dbe2f1830
tod619 <trevorodwyer619@hotmail.com>
2023-07-17 22:17:52 +0100

    add flash card img + title + word to canvas in imain.py file

--- main.py
+++ main.py
@@ -4,8 +4,18 @@ from tkinter import *
 
 BACKGROUND_COLOR = "#B1DDC6"
 
+# Create Window
 window = Tk()
 window.title("Flash Cards")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+# Add card image to canvas
+canvas = Canvas(width=800, height=526)
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+card_front_img = PhotoImage(file="images/card_front.png")
+canvas.create_image(400, 263, image=card_front_img)
+canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
+canvas.create_text(400, 263, text="Word", font=("Ariel", 60, "bold"))
+canvas.grid(row=0, column=0)
 
 window.mainloop()
