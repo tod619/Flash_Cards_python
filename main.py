51ad86345f46b585d0f4de123b4ae258dbe06bdd
tod619 <trevorodwyer619@hotmail.com>
2023-07-18 22:27:48 +0100

    reset flip_timer every time next_card func is called in main.py file

--- main.py
+++ main.py
@@ -16,11 +16,13 @@ current_card = {}
 
 
 def next_card():
-    global current_card
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
     current_card = random.choice(to_learn)
     canvas.itemconfig(card_title, text="French", fill="black")
     canvas.itemconfig(card_word, text=current_card["French"], fill="black")
     canvas.itemconfig(card_background, image=card_front_img)
+    flip_timer = window.after(3000, func=flip_card)
 
 # Flip Card Function
 
@@ -38,7 +40,7 @@ window.config(padx=40, pady=40, bg=BACKGROUND_COLOR)
 
 
 # Flip Card after 3 seconds
-window.after(3000, func=flip_card)
+flip_timer = window.after(3000, func=flip_card)
 
 # Add card image to canvas
 canvas = Canvas(width=800, height=526)
